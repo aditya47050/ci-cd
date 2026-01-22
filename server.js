19a27efdd161ced4a202f0441a02af6e047703f5
aditya47050 <163160914+aditya47050@users.noreply.github.com>
2026-01-22 23:13:38 +0530

    newchanges

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 app.get('/',(req,res)=>{
     res.json({
         status:200,
-        message:"hello world"
+        message:"Hello ! this is my first ci-cd pipeline"
     })
 })
 
